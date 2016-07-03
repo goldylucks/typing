f8e865745be2eb5a017fcf5f6265fd1a79abfc8c
goldylucks <adamgoldman7@gmail.com>
2016-07-03 02:25:46 +0200

    calculate accuracy
    reolves #5

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -3,24 +3,42 @@ import $ from 'jquery';
 export default class Stats {
   init () {
     this.$wpm = $('#wpm');
+    this.$accuracy = $('#accuracy');
     this.started = Date.now();
-    this.interval = setInterval(::this.wpmInterval, 100);
+    this._interval = setInterval(::this.interval, 100);
   }
 
-  wpmInterval () {
+  interval () {
     this.$wpm.text(this.calcWpm());
+    this.$accuracy.text(this.calcAccuracy());
   }
 
   calcWpm () {
-    return parseInt(this.getCorrectKeys().length / this.calcSecondsElapsed() * 60 / 4);
+    return parseInt(
+      this.getCorrectCount() / this.calcSecondsElapsed() * 60 / 4
+    );
+  }
+
+  calcAccuracy () {
+    const dirtyCount = this.getDirtyCount();
+    if (!dirtyCount) {
+      return 0;
+    }
+    return parseInt(
+      this.getCorrectCount() / dirtyCount * 100
+    );
   }
 
   calcSecondsElapsed () {
     return (Date.now() - this.started) / 1000;
   }
 
-  getCorrectKeys () {
-    return $('.letter.correct');
+  getCorrectCount () {
+    return $('.letter.correct').length;
+  }
+
+  getDirtyCount () {
+    return $('.letter.dirty').length;
   }
 
 }
