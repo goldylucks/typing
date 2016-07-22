fd1a3aa97bf8bf2ab605f078c5f7874d9f431471
goldylucks <adamgoldman7@gmail.com>
2016-07-22 18:34:46 +0200

    (dev): add shout method

--- server/config/development.js
+++ server/config/development.js
@@ -1,3 +1,9 @@
+global.SHOUT = function SHOUT (...args) {
+  console.log('*********************');
+  console.log.apply(null, args);
+  console.log('*********************');
+};
+
 module.exports = {
 
   logging: true,
