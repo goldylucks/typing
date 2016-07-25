9681bd5900c6028cce5c21744cd78961f3ff9a36
goldylucks <adamgoldman7@gmail.com>
2016-07-25 10:02:09 +0200

    (tests): fix e2e

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,6 +1,6 @@
 module.exports = {
   'src_folders': ['./test/e2e'],
-  'output_folder': 'reports',
+  'output_folder': false,
   'custom_commands_path': '',
   'custom_assertions_path': '',
   'page_objects_path': '',
