043b681d57c9c199de59dbed5535012e66ea8f5c
goldylucks <adamgoldman7@gmail.com>
2016-07-27 00:52:44 +0200

    (chore): deploy to gh-pages on success
    #10 WIP

--- scripts/deploy-to-github-pages.sh
+++ scripts/deploy-to-github-pages.sh
@@ -6,6 +6,7 @@ TARGET_BRANCH="gh-pages"
 GIT_USER_EMAIL="admin@travis-ci.org"
 GIT_USER_NAME="Travis CI"
 BUILD_PATH="build"
+TEST_REPO_SLUG="goldylucks/typing-test"
 SHA=$(git rev-parse --verify HEAD) # reference to commit deployed to TARGET_BRANCH
 COMMIT_MSG="Deploy to Github Pages: ${SHA}"
 
@@ -27,4 +28,15 @@ git init
 git add .
 git commit -m "$COMMIT_MSG"
 
-git push -f --quiet "https://${GITHUB_TOKEN}@github.com/${REPO_SLUG}.git" $SOURCE_BRANCH:$TARGET_BRANCH > /dev/null 2>&1
+# no tags, deploy to test app
+if [ -z "$TRAVIS_TAG"]; then
+  echo "No tag detected"
+  echo "deploying to test: ${TEST_REPO_SLUG}"
+  git push -f --quiet "https://${GITHUB_TOKEN}@github.com/${TEST_REPO_SLUG}.git" $SOURCE_BRANCH:$TARGET_BRANCH > /dev/null 2>&1
+  exit 0
+fi
+
+# tags, release baby! deploy to LIVE app
+echo "Tag detected: ${TRAVIS_TAG}"
+echo "deploying to production: ${TRAVIS_REPO_SLUG}"
+git push -f --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" $SOURCE_BRANCH:$TARGET_BRANCH > /dev/null 2>&1
