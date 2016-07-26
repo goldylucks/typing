cf600b341df2464426d06a4eb19a1dcae5e00bb5
goldylucks <adamgoldman7@gmail.com>
2016-07-26 17:49:55 +0200

    (MainPage): comment out renderAuth

--- client/pages/MainPage/MainPage.js
+++ client/pages/MainPage/MainPage.js
@@ -12,7 +12,7 @@ export default class MainPage {
 
   init () {
     this.render();
-    this.renderAuth();
+    // this.renderAuth();
     $(document).on('click', '#submit', this.onAdd);
     $(document).on('click', '#public', this.onPublicToggle);
   }
