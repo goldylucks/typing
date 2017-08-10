7464298d497846a452bb161d6c7b2913a34ffd01
brainwithE <haylyzh@gmail.com>
2017-08-11 01:31:11 +0800

    refactor(header): implement react-router-dom

--- src/shared/components/Header.jsx
+++ src/shared/components/Header.jsx
@@ -1,19 +1,27 @@
 // @flow
 
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = () =>
   <nav className="navbar navbar-default">
     <div className="container">
       <div className="navbar-header">
-        <a className="navbar-brand" href="./">Typing</a>
+        <Link to="/" className="navbar-brand">Typing</Link>
       </div>
       <ul className="nav navbar-nav navbar-right">
-        <li><a href="add-text">Add</a></li>
-        <li><a href="nav">Texts</a></li>
-        <li><a href="history">History</a></li>
-        <li><a>Login</a></li>
-        <li><a>Logout</a></li>
+        {[
+          { route: '/add-text', label: 'Add' },
+          { route: '/texts', label: 'Texts' },
+          { route: '/history', label: 'History' },
+          { route: '/#login', label: 'Login' },
+          { route: '/#logout', label: 'Logout' },
+        ].map(link => (
+          <li className="nav-item" key={link.route}>
+            <NavLink to={link.route} className="nav-link" activeStyle={{ color: 'white' }} exact>{link.label}</NavLink>
+          </li>
+        ))}
+
       </ul>
     </div>
   </nav>
